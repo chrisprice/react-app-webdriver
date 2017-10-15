7b72b5fb838a2902d503c766fcb8e9c149b6e3fb
Avindra Goolcharan <aavindraa@gmail.com>
2017-10-15 00:12:37 -0400

    setupTests.js: include working teardown example
    this adds a teardown option to the `setupTests.js`

--- e2e/setupTests.js
+++ e2e/setupTests.js
@@ -1,3 +1,14 @@
+import { driver } from './helpers';
+
+// Tear down chrome Driver when tests
+// are concluded.
+
+afterAll((done) => {
+  driver.quit().then(() => done());
+});
+
+// set defaults
+
 process.env.USE_PROMISE_MANAGER = false;
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 60e3;
